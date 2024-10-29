f2174800c724a41415f83e2674e581df59f101ad
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2024-10-29 11:20:47 -0300

    spring-batch-performance: Criado projeto para Steps Paralelos com jobs

---
+++ ParallelStepsJob/src/main/java/com/springbatch/parallelsteps/ParallelStepsJobApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.parallelsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelStepsJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelStepsJobApplication.class, args);
+	}
+
+}
---
+++ ParallelStepsJob/src/test/java/com/springbatch/jsonreader/ParallelStepsJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.jsonreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelStepsJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
